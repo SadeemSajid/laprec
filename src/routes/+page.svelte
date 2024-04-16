7d40e0208be2877cb18d18c956a651526f434a7a
Sadeem Sajid <sadeem@KALAMEET.Home>
2024-04-16 14:24:21 +0500

    feat: svelteKit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import '../app.postcss';
+</script>
+
+<div class="flex justify-center join mt-12">
+
+    <div class="join-item border border-black px-12 rounded-none flex justify-center flex-col">
+        <h1>Welcome to LapRec</h1>
+        <p>Please navigate to <a href="/search">Search</a> for the website.</p>
+    </div>
+
+    <div class="join-item rounded-none">
+        <img class="w-[700px]" src="https://images.unsplash.com/photo-1693408714642-a5dc17ff70e9?q=80&w=1937&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Beautiful Background">
+    </div>
+
+</div>
