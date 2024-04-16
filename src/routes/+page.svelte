c5d9feedabeb54d18cbb7e64ac6a085898959d48
Sadeem Sajid <sadeem@KALAMEET.Home>
2024-04-16 21:23:44 +0500

    feat: search page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,22 @@
     import '../app.postcss';
 </script>
 
-<div class="flex justify-center join mt-12">
+<div class="flex justify-center">
 
-    <div class="join-item border border-black px-12 rounded-none flex justify-center flex-col">
-        <h1>Welcome to LapRec</h1>
-        <p>Please navigate to <a href="/search">Search</a> for the website.</p>
-    </div>
-
-    <div class="join-item rounded-none">
-        <img class="w-[700px]" src="https://images.unsplash.com/photo-1693408714642-a5dc17ff70e9?q=80&w=1937&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Beautiful Background">
+    <div class="join mt-12 shadow-2xl">
+        <div class="join-item border border-black px-12 rounded-none flex flex-col justify-center w-96">
+            <h2 class="mb-4">Please navigate to <a href="/search">Search</a> for the website.</h2>
+            <p class="text-xl my-4">Designed By</p>
+            <div class="flex justify-start gap-4">
+                <p>Sadeem</p>
+                <p>Abdullah</p>
+                <p>Hassan</p>
+            </div>
+        </div>
+    
+        <div class="join-item rounded-none">
+            <img class="w-[700px]" src="https://images.unsplash.com/photo-1693408714642-a5dc17ff70e9?q=80&w=1937&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Beautiful Background">
+        </div>
     </div>
 
 </div>
---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,8 @@
+<div class="flex justify-center h-screen items-center">
+    <label class="input input-bordered flex items-center  gap-2 w-[60rem]">
+        <input type="text" class="grow" placeholder="Search" />
+        <kbd class="kbd kbd-m">⌘</kbd>
+        <kbd class="kbd kbd-m">K</kbd>
+    </label>
+</div>
+
